b4a2ae4e4073a1318f649fcf7d7e6106c0c26f57
aguirre739 <pame11_@live.com>
2020-06-21 23:40:14 -0300

    se agregan las validaciones en los campos

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,23 +2,35 @@
 function sumar() {
     let num1 = parseInt(document.getElementById("numero1").value);
     let num2 = parseInt(document.getElementById("numero2").value);
-    let resultado = num1 + num2;
-    document.getElementById("resultado").innerText = resultado;
-    console.log(resultado);
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById("resultado").innerText = "Ingrese solo números para poder operar"
+    } else{
+        let resultado = num1 + num2;
+        document.getElementById("resultado").innerText = resultado;
+        console.log(resultado);
+    }
 }
 
 function restar() {
     let num1 = parseInt(document.getElementById("numero1").value);
     let num2 = parseInt(document.getElementById("numero2").value);
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById("resultado").innerText = "Ingrese solo números para poder operar"
+    } else{
     let resultado = num1 - num2;
     document.getElementById("resultado").innerText = resultado;
+    }
 }
 
 function multiplicar() {
     let num1 = parseInt(document.getElementById("numero1").value);
     let num2 = parseInt(document.getElementById("numero2").value);
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById("resultado").innerText = "Ingrese solo números para poder operar"
+    } else{
     let resultado = num1 * num2;
     document.getElementById("resultado").innerText = resultado;
+    }
 }
 
 function dividir() {
@@ -27,6 +39,8 @@ function dividir() {
     if (num2 == 0) {
         let resultado = document.getElementById("resultado");
         resultado.innerText = "No se puede dividir por cero (0)"
+    } else if(isNaN(num1) || isNaN(num2)){
+        document.getElementById("resultado").innerText = "Ingrese solo números para poder operar"
     } else {
         let resultado = num1 / num2;
         document.getElementById("resultado").innerText = resultado;
